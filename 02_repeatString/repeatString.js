54f4b704bf489124ede9024ed76b3c38459ddb65
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-01-27 22:27:01 -0500

    Complete exercise 1 through 4
    Update code to pass all tests for first four

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let builtString="";
+    if (num <0) return "ERROR";
+    for (let i=0; i < num; i++){
+        builtString+=string;
+    }
+    return builtString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
 const removeFromArray = function() {
+    let baseArray = arguments[0];
+    let searchValues = [];
+    for (var i = 1; i < arguments.length; i++){
+        searchValues.push(arguments[i]);
+    }
+    for (let i =0; i<searchValues.length; i++){
+        for (let j=0; j<baseArray.length; j++){
+            if (searchValues[i]===baseArray[j]){
+                baseArray.splice(j,1);
+                j--;
+            }
+        }
+    }
+    return baseArray;
 };
 
 // Do not edit below this line
