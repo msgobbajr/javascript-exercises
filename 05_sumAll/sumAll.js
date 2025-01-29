5197fa3b3b87db7ccabefe0108ac1ce72f460641
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-01-28 21:15:03 -0500

    Finished all required exercises
    Code for exercise 5-7 in odin project

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum=0;
+    let intOne = 0;
+    let intTwo = 0;
+    if (first<second){
+        intOne=first;
+        intTwo=second;
+    }
+    else {
+        intTwo=first;
+        intOne=second;
+    }
 
+    if (intOne < 0 || intTwo < 0) return 'ERROR';
+    else if (intOne%1!=0 || intTwo%1!=0) return 'ERROR';
+    else if (typeof(intOne)!="number" || typeof(intTwo)!="number") return 'ERROR';
+
+    for (;intOne<=intTwo;intOne++){
+        sum+=intOne;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  fTemp = (fTemp-32)*(5/9);
+  if (fTemp%1 != 0) return Number(fTemp.toFixed(1));
+  else return fTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  cTemp = (cTemp*(9/5))+32;
+  if (cTemp%1 != 0) return Number(cTemp.toFixed(1));
+  else return cTemp;
 };
 
 // Do not edit below this line
