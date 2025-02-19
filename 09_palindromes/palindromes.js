fd958d8b3cd5d0cbb8e50a9d65b4e9e180dd7a4e
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-02-19 17:16:48 -0500

    Added code for 9
    Updated function to check for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replaceAll(' ','');
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    str = str.toLowerCase();
+    let a = 0;
+    let b = str.length-1;
+    while (a!=b && a<b){
+        if(str[a]!=str[b]){
+            return false;
+        }
+        a++;
+        b--;
+    }
+    return true;
 };
 
 // Do not edit below this line
