089843138b3f15893736e8af3d7b8411cbdc5292
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-02-19 16:12:02 -0500

    Finish part 8
    Added functions for completion of calcuator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum,current)=> sum + current,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum,current)=> sum * current,1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let sum = a;
+  b--;
+  while (b > 0){
+    sum *= a;
+    b--;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let sum = 1;
+  while (a > 0){
+    sum *= a;
+    a--;
+  }
+  return sum;
 };
 
 // Do not edit below this line
