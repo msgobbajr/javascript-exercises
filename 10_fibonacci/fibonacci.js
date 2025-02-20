71b4979d81ba67d8cc9bc1acd7f1e4d6183a1c4e
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-02-19 20:28:52 -0500

    Added code for 10
    Added function for fibonacci calculator

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0 ) return "OOPS";
+    let arr = [0,1];
+    for (let i=2;i<=num;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr[num];
 };
 
 // Do not edit below this line
