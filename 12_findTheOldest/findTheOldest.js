8653ab92a9e7bc10e6de0605ad92e5bfd7ef3590
Mark Sgobba <74211472+msgobbajr@users.noreply.github.com>
2025-02-26 21:52:21 -0500

    Added code for 12
    Finished evaluating age of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let index = undefined;
+    array.reduce(function(highest, current, place){
+        if (current.yearOfDeath === undefined){
+            if (highest < (new Date().getFullYear() - current.yearOfBirth)){
+                highest = new Date().getFullYear() - current.yearOfBirth;
+                index = place;
+            }
+        }
+        if (highest < (current.yearOfDeath - current.yearOfBirth)){
+            highest = current.yearOfDeath - current.yearOfBirth;
+            index = place;
+        }
+        return highest;
+    }, 0);
+    return array[index];
 };
 
 // Do not edit below this line
